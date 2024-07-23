08db7d1b326ee53e01b25543440a870057594d87
Jean <JFBimCopinski@gmail.com>
2024-07-23 08:32:46 -0300

    feat: lógica de entrada

---
+++ index.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")()
+
+while (true){
+console.log(`
+Tecle 1 para adicionar uma tarefa.
+Tecle 2 para ver suas tarefas.
+Tecle 3 para deletar sua tarefa.
+Tecle 4 para atualizar sua tarefa.
+Tecle 5 para sair.`)
+let entrada = +prompt("O que deseja realizar? ")
+switch (entrada) {
+    case "1":
+        break;
+    case "2":
+        break
+    case "3":
+        break
+    case "4":
+        break
+    case "5":
+        console.log("Saindo...")
+        process.exit()
+        break
+}
+}
