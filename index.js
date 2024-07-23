2d8b550b246dd8ed8d5bb8f2c90b69b3fbffbffa
Jean <JFBimCopinski@gmail.com>
2024-07-23 08:34:47 -0300

    feat: lógica de leitura de input

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const prompt = require("prompt-sync")()
 
 while (true){
-console.log(`
-Tecle 1 para adicionar uma tarefa.
+console.log(`Tecle 1 para adicionar uma tarefa.
 Tecle 2 para ver suas tarefas.
 Tecle 3 para deletar sua tarefa.
 Tecle 4 para atualizar sua tarefa.
 Tecle 5 para sair.`)
 let entrada = +prompt("O que deseja realizar? ")
 switch (entrada) {
-    case "1":
+    case 1:
+        console.log("adicionar")
         break;
-    case "2":
+    case 2:
+        console.log("ver")
         break
-    case "3":
+    case 3:
+        console.log("deletar")
         break
-    case "4":
+    case 4:
+        console.log("atualizar")
         break
-    case "5":
+    case 5:
         console.log("Saindo...")
         process.exit()
         break
